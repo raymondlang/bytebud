510b11c02a13c434283b7262bd8f6a90dd306e53
Raymond <raymond.w.lang@gmail.com>
2024-01-28 13:25:10 -0800

    attemptmodal menu item styling

--- react-app/src/components/FriendsList/index.js
+++ react-app/src/components/FriendsList/index.js
@@ -1,5 +1,9 @@
 import { Redirect } from "react-router";
 import { useHistory } from "react-router-dom";
+import { useState } from "react";
+import OpenModalMenuItem from "../EmojisModal/OpenModalMenuItem";
+import GetAllEmojis from "../EmojisModal";
+import "../EmojisModal/GetAllEmojis.css";
 
 export default function FriendsList() {
   const history = useHistory();
@@ -9,10 +13,18 @@ export default function FriendsList() {
     history.push("/emojis/test");
   };
 
+  const [showMenu, setShowMenu] = useState(false);
+  const closeMenu = () => setShowMenu(false);
+
   return (
     <div>
       <h1> HELLO ! </h1>
-      <button onClick={redirect}> Emojis</button>
+      <OpenModalMenuItem
+        itemText="Emojis"
+        onItemClick={closeMenu}
+        className="emojis-modal-button"
+        modalComponent={<GetAllEmojis />}
+      />
     </div>
   );
 }
