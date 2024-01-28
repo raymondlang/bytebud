1a208588a124e935cfa6708fdd05368367b91d25
Raymond <raymond.w.lang@gmail.com>
2024-01-28 13:29:13 -0800

    add html to signuppage

--- react-app/src/components/SignupFormPage/index.js
+++ react-app/src/components/SignupFormPage/index.js
@@ -4,80 +4,103 @@ import { Redirect } from "react-router-dom";
 import { signUp } from "../../store/session";
 import "./SignupForm.css";
 
-function SignupFormPage() {
+function SignupPage() {
   const dispatch = useDispatch();
   const sessionUser = useSelector((state) => state.session.user);
   const [email, setEmail] = useState("");
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
-  const [confirmPassword, setConfirmPassword] = useState("");
+  const [month, setMonth] = useState("");
+  const [day, setDay] = useState("");
+  const [year, setYear] = useState("");
   const [errors, setErrors] = useState([]);
 
   if (sessionUser) return <Redirect to="/channels/@me" />;
 
   const handleSubmit = async (e) => {
     e.preventDefault();
-    if (password === confirmPassword) {
-      const data = await dispatch(signUp(username, email, password));
-      if (data) {
-        setErrors(data);
-      }
-    } else {
-      setErrors([
-        "Confirm Password field must be the same as the Password field",
-      ]);
+  };
+
+  const generateOptions = (start, end) => {
+    const options = [];
+    for (let i = start; i <= end; i++) {
+      options.push(
+        <option key={i} value={i}>
+          {i}
+        </option>
+      );
     }
+    return options;
   };
 
   return (
-    <>
-      <h1>Sign Up</h1>
-      <form onSubmit={handleSubmit}>
-        <ul>
-          {errors.map((error, idx) => (
-            <li key={idx}>{error}</li>
-          ))}
-        </ul>
-        <label>
-          Email
-          <input
-            type="text"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-            required
-          />
+    <div className="signup-container">
+      <h1 className="signup-header">Create an account</h1>
+      <form className="signup-form" onSubmit={handleSubmit}>
+        <label htmlFor="email" className="signup-input-label">
+          EMAIL
         </label>
-        <label>
-          Username
-          <input
-            type="text"
-            value={username}
-            onChange={(e) => setUsername(e.target.value)}
-            required
-          />
+        <input
+          type="email"
+          id="signup-email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          className="signup-input signup-email"
+        />
+        <label htmlFor="username" className="signup-input-label">
+          USERNAME
         </label>
-        <label>
-          Password
-          <input
-            type="password"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-            required
-          />
+        <input
+          type="text"
+          value={username}
+          onChange={(e) => setUsername(e.target.value)}
+          className="signup-input signup-username"
+        />
+        <label htmlFor="password" className="signup-input-label">
+          PASSWORD
         </label>
-        <label>
-          Confirm Password
-          <input
-            type="password"
-            value={confirmPassword}
-            onChange={(e) => setConfirmPassword(e.target.value)}
-            required
-          />
-        </label>
-        <button type="submit">Sign Up</button>
+        <input
+          type="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          className="signup-input signup-password"
+        />
+        <div className="signup-dob">
+          <select
+            id="month"
+            value={month}
+            onChange={(e) => setMonth(e.target.value)}
+            className="signup-dob-select"
+          >
+            <option value="">Month</option>
+            {generateOptions(1, 12)}
+          </select>
+          <select
+            id="day"
+            value={day}
+            onChange={(e) => setDay(e.target.value)}
+            className="signup-dob-select"
+          >
+            <option value="">Day</option>
+            {generateOptions(1, 31)}
+          </select>
+          <select
+            id="year"
+            value={year}
+            onChange={(e) => setYear(e.target.value)}
+            className="signup-dob-select"
+          >
+            <option value="">Year</option>
+            {generateOptions(1900, new Date().getFullYear())}
+          </select>
+        </div>
+        <button type="submit" className="signup-submit-btn">
+          Continue
+        </button>
       </form>
-    </>
+      <span className="signup-already-account">Already have an account?</span>
+    </div>
   );
 }
 
-export default SignupFormPage;
+export default SignupPage;
