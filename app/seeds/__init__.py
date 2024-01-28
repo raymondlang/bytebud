137e9801fa7d3733e248df984cf183ff66b5cfc8
Raymond <raymond.w.lang@gmail.com>
2024-01-27 16:34:40 -0800

    fixed seeder files

--- app/seeds/__init__.py
+++ app/seeds/__init__.py
@@ -22,16 +22,20 @@ def seed():
         # command, which will  truncate all tables prefixed with
         # the schema name (see comment in users.py undo_users function).
         # Make sure to add all your other model's undo functions below
-        undo_servers()
+        undo_reactions()
+        undo_emojis()
         undo_messages()
+        undo_channels()
+        undo_servers()
+        undo_friends()
         undo_users()
-    demo, marnie, bobbie = seed_users()
     seed_users()
     seed_friends()
     seed_servers()
     seed_channels()
-    undo_friends()
     seed_messages()
+    seed_emojis()
+    seed_reactions()
 
     # Add other seed functions here
 
