976709083bc0f9c39afe75ddbd350605dc7baac1
Raymond <raymond.w.lang@gmail.com>
2024-01-28 13:05:47 -0800

    progressed emoji slice in redux store

--- react-app/src/components/EmojisModal/index.jsx
+++ react-app/src/components/EmojisModal/index.jsx
@@ -1,9 +1,63 @@
-import React, { useState } from "react";
-import { useDispatch } from "react-redux";
+import React, { useState, useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
 import { useModal } from "../../context/Modal";
+import OpenModalMenuItem from "./OpenModalMenuItem";
+import {
+  getAllEmojisThunk,
+  createReactionThunk,
+  loadOneEmojiThunk,
+  allEmojis,
+} from "../../store/emojis";
 import "./GetAllEmojis.css";
 
-function GetAllEmojis() {
+// to be put into message component
+// const [showMenu, setShowMenu] = useState(false);
+// const closeMenu = () => setShowMenu(false);
+
+export default function GetAllEmojis() {
   const dispatch = useDispatch();
-  return <h1> hello !</h1>;
+  const { closeModal } = useModal();
+
+  useEffect(() => {
+    dispatch(getAllEmojisThunk());
+  }, [dispatch]);
+
+  const emojis = useSelector((state) => state.emoji.allEmojis);
+  const emojisArr = Object.values(emojis);
+
+  console.log("emojis arr", emojisArr);
+
+  const userId = useSelector((state) => state.session.user?.id);
+
+  const handleClick = (e, emojiId, messageId) => {
+    // get create a reaction from the click
+    // need to input messageId from message component
+    let new_reaction = dispatch(
+      createReactionThunk(emojiId, messageId, userId)
+    );
+
+    // query for the emoji if the reaction successfully worked
+    if (new_reaction) {
+      let emoji = dispatch(loadOneEmojiThunk(new_reaction["emojiId"]));
+      new_reaction[emoji.id] = emoji.url // replacing emoji Id with emojiURL?
+        .then(closeModal);
+    }
+  };
+
+  return (
+    <div className="emoji-modal-container">
+      <p>{String.fromCodePoint(0x1f354)}</p>
+      {emojisArr.map((emoji) => {
+        return (
+          <p
+            className="emoji-modal-emoji"
+            value={emoji.id}
+            onClick={handleClick}
+          >
+            {String.fromCodePoint(emoji.url)}
+          </p>
+        );
+      })}
+    </div>
+  );
 }
--- react-app/src/store/emojis.js
+++ react-app/src/store/emojis.js
@@ -1,6 +1,7 @@
 // Constants
 const LOAD_EMOJIS = "emojis/LOAD_EMOJIS";
 const LOAD_ONE_EMOJI = "emojis/LOAD_ONE_EMOJI";
+const CREATE_REACTION = "emojis/CREATE_REACTIONS";
 
 // Action Creators
 const loadEmojis = (emojis) => ({
@@ -11,6 +12,11 @@ const loadEmojis = (emojis) => ({
 const loadOneEmoji = (emoji) => ({
   type: LOAD_ONE_EMOJI,
   emoji,
+});
+
+const createReaction = (reaction) => ({
+  type: CREATE_REACTION,
+  reaction,
 });
 
 // Thunks
@@ -34,23 +40,40 @@ export const loadOneEmojiThunk = (id) => async (dispatch) => {
   }
 };
 
+export const createReactionThunk = (reactionData) => async (dispatch) => {
+  const response = await fetch("/api/emojis", {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify(reactionData),
+  });
+  if (response.ok) {
+    const newReaction = await response.json();
+    // going to need to do this within the messages slice of state
+    // Query for the emoji based on this reaction!
+    // add the new reaction to the message slice of state
+    dispatch(createReaction(newReaction));
+    return newReaction; // will return the emoji once we get there
+  }
+};
+
 // reducer
 
 let initialState = {
   allEmojis: {},
+  emoji: {},
 };
 
 export default function emojisReducer(state = initialState, action) {
   let newState = {};
   switch (action.type) {
     case LOAD_EMOJIS:
-      newState = { ...state, allEmojis: {} };
+      newState = { ...state, allEmojis: {}, emoji: { ...state.emoji } };
       action.emojis.emojis.forEach(
         (emoji) => (newState.allEmojis[emoji.id] = emoji)
       );
       return newState;
     case LOAD_ONE_EMOJI:
-      newState = { ...state, allEmojis: { ...state.allEmojis } };
+      newState = { ...state, allEmojis: { ...state.allEmojis }, emoji: {} };
       newState.emoji = action.emoji;
       return newState;
     default:
