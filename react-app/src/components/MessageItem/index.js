57631ff10c115b17928653a794743522b5da497d
Raymond <raymond.w.lang@gmail.com>
2024-01-29 15:18:12 -0800

    start messageitem

---
+++ react-app/src/components/MessageItem/index.js
@@ -0,0 +1,16 @@
+import React from "react";
+import "./MessageItem.css";
+
+function MessageItem({ message }) {
+  //  .slice(0, -5) on username to get rid of #tag
+
+  return (
+    <>
+      <div className="message-sender">
+        {/* <div key={ind}>{`${} ${message.msg}`}</div> */}
+        <p>{message}</p>
+      </div>
+    </>
+  );
+}
+export default MessageItem;
