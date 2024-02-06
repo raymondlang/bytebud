9d738035c88d8235a83e9f375fc22c72e9b11b91
Raymond <raymond.w.lang@gmail.com>
2024-02-05 20:28:54 -0800

    gear added

--- react-app/src/components/ChannelSideBar/index.js
+++ react-app/src/components/ChannelSideBar/index.js
@@ -58,28 +58,34 @@ function Channels() {
         </div>
       </div>
       {allChannels.map((channel) => (
-        <Link
-          key={channel.id}
-          to={`/channels/${channel.serverId}/${channel.id}`}
-          className={`channel-divs${
-            channel.id === currChannel?.id ? " selected" : ""
-          }`}
-        >
-          <span
-            className={`hashtag${
+        <div className="channel-mapping">
+          <Link
+            key={channel.id}
+            to={`/channels/${channel.serverId}/${channel.id}`}
+            className={`channel-divs${
               channel.id === currChannel?.id ? " selected" : ""
             }`}
           >
-            #
-          </span>
-          <span
-            className={`channel-text-name${
-              channel.id === currChannel?.id ? " selected" : ""
-            }`}
-          >
-            {channel.name}
-          </span>
-        </Link>
+            <span
+              className={`hashtag${
+                channel.id === currChannel?.id ? " selected" : ""
+              }`}
+            >
+              #
+            </span>
+            <span
+              className={`channel-text-name${
+                channel.id === currChannel?.id ? " selected" : ""
+              }`}
+            >
+              {channel.name}
+            </span>
+          </Link>
+          <OpenModalButton
+            buttonText={<i class="fa-solid fa-gear"></i>}
+            modalComponent={<NewChannel serverId={serverId} />}
+          />
+        </div>
       ))}
     </div>
   );
