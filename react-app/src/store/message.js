e260bcffd930810b65499af645450f0c4d1d3a2f
Raymond <raymond.w.lang@gmail.com>
2024-02-12 19:01:31 -0800

    refactor message slice's getMessage getChannelMessages fx

--- react-app/src/store/message.js
+++ react-app/src/store/message.js
@@ -41,43 +41,15 @@ const deleteReaction = (reactionId, messageId) => ({
 // thunk action creators:
 
 export const getChannelMessages = (channelId) => async (dispatch) => {
-  const resMessages = await fetch(`/api/channels/${channelId}/messages`);
-
+  let resMessages;
+  try {
+    resMessages = await fetch(`/api/channels/${channelId}/messages`);
+  } catch (error) {
+    console.error("Failed to fetch channel messages", error);
+  }
   if (resMessages.ok) {
-    // Parse the JSON response to get all messages with their reactions
     const channelMessages = await resMessages.json();
-
-    // Extract all the emoji IDs from the reactions in the messages
-    const reactions = channelMessages.flatMap((message) =>
-      message.reactions.map((reaction) => reaction.emojiId)
-    );
-
-    // Fetch all the distinct emojis in parallel using Promise.all()
-    const emojiUrls = await Promise.all(
-      [...new Set(reactions)].map((emojiId) =>
-        fetch(`/api/emojis/${emojiId}`).then((res) =>
-          res.ok ? res.json() : null
-        )
-      )
-    );
-
-    // Create a map of emoji IDs to their URLs for faster lookup
-    const emojiMap = emojiUrls.reduce(
-      (map, emoji) => (emoji ? { ...map, [emoji.id]: emoji.url } : map),
-      {}
-    );
-
-    // Map over the channel messages and replace each reaction's emojiId with its corresponding emojiURL
-    const messagesWithEmojis = channelMessages.map((message) => ({
-      ...message,
-      reactions: message.reactions.map((reaction) => ({
-        ...reaction,
-        emojiURL: emojiMap[reaction.emojiId] || "",
-      })),
-    }));
-
-    // Dispatch the updated messages with emoji URLs to the store
-    dispatch(loadMessages(messagesWithEmojis));
+    dispatch(loadMessages(channelMessages));
   }
 };
 
