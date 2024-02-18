31c3d3498a36215eb114388bfdb08fc2a5f8ccb1
Raymond <raymond.w.lang@gmail.com>
2024-02-18 00:10:59 -0800

    added 404 page

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,31 +1,30 @@
 import React, { useState, useEffect } from "react";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { Route, Switch } from "react-router-dom";
 import SignupFormPage from "./components/SignupFormPage";
 import { authenticate } from "./store/session";
 import LoginPage from "./components/LoginPage";
 import ServersSidebar from "./components/Servers/ServersSidebar";
+import ChannelSideBar from "./components/ChannelSideBar";
 import SplashPage from "./components/SplashPage";
 // import Home from "./components/Home/"
 import FriendsList from "./components/FriendsList";
 import MessageForm from "./components/MessageForm";
-// import GetAllEmojis from "./components/EmojisModal";
 import ChannelTopBar from "./components/ChannelTopBar";
 import UserMenu from "./components/UserMenu";
-import ServerMembers from "./components/ServerMembers";
-
-import ChannelSideBar from "./components/ChannelSideBar";
+import NotFoundPageLoggedOut from "./components/NotFoundPageLoggedOut";
+import NotFoundPageLoggedIn from "./components/NotFoundPageLoggedIn";
 
 function App() {
   const dispatch = useDispatch();
   const [isLoaded, setIsLoaded] = useState(false);
+  const sessionUser = useSelector((state) => state.session.user);
+
   useEffect(() => {
     dispatch(authenticate()).then(() => setIsLoaded(true));
   }, [dispatch]);
-
   return (
     <>
-      <LoginPage />
       <Switch>
         <Route exact path="/">
           <SplashPage />
@@ -36,26 +35,27 @@ function App() {
         <Route exact path="/register">
           <SignupFormPage />
         </Route>
+        <Route>
+          <NotFoundPageLoggedOut sessionUser={sessionUser} />
+        </Route>
       </Switch>
       {isLoaded && (
         <>
           <ServersSidebar />
           <Switch>
-            <Route path="/channels/@me">
+            <Route exact path="/channels/@me">
               <FriendsList />
+              <UserMenu />
             </Route>
-            <Route path="/channels/:serverId/:channelId">
+            <Route exact path="/channels/:serverId/:channelId">
               <ChannelSideBar />
               <ChannelTopBar />
               <MessageForm />
               <UserMenu />
-              <ServerMembers />
             </Route>
-            {/* for testing messages ONLY */}
-            <Route path="/messages">
-              <MessageForm />
+            <Route>
+              <NotFoundPageLoggedIn sessionUser={sessionUser} />
             </Route>
-            <Route path="/emojis/test">{/* <GetAllEmojis /> */}</Route>
           </Switch>
         </>
       )}
